3e902b5faa7dd5b4b5258fb8e74dba78b12aa0af
prakashsrv <prakashsaurav123@gmail.com>
2019-11-19 17:20:42 +0530

    added navigation drawer

--- app/src/main/java/com/pack/cryptomvvm/BaseApplication.kt
+++ app/src/main/java/com/pack/cryptomvvm/BaseApplication.kt
@@ -1,12 +1,25 @@
 package com.pack.cryptomvvm
 
 import android.app.Application
+import com.pack.cryptomvvm.di.DaggerSharedComponent
+import com.pack.cryptomvvm.di.SharedComponent
+import com.pack.cryptomvvm.di.SharedModule
 
 class BaseApplication:Application() {
+
+    lateinit var component:SharedComponent
 
 
     override fun onCreate() {
         super.onCreate()
+
+        component=DaggerSharedComponent.builder().sharedModule(SharedModule(this)).build()
+    }
+
+
+    fun getSharedComponent():SharedComponent{
+
+        return component
     }
 
 }
